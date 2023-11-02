443213e4709668a9ab2c5c9618597940a1623482
Wolfgang Fahl <wf@bitplan.com>
2023-11-02 11:23:19 +0100

    add lod_index to lod_grid

--- ngwidgets/lod_grid.py
+++ ngwidgets/lod_grid.py
@@ -6,6 +6,7 @@ Created on 2023-10-3
 import datetime
 from nicegui import ui
 import asyncio
+import sys
 
 class ListOfDictsGrid:
     """
@@ -15,15 +16,19 @@ class ListOfDictsGrid:
     
     """
 
-    def __init__(self,lod=None,columnDefs:list=None,options={},theme="ag-theme-material",classes="h-80"):
+    def __init__(self,lod=None,key_col:str="#",columnDefs:list=None,options={},theme="ag-theme-material",classes="h-80",lenient:bool=False):
         """
         Constructor
         """
         self.lod=lod
+        self.key_col=key_col
+        self.lenient=lenient
+        self.lod_index = {}
         self.ag_grid=ui.aggrid(options).classes(classes)
         self.ag_grid.theme=theme 
         self.setDefaultColDef()
         ui.button("resize",on_click=self.onSizeColumnsToFit)
+        ui.button('Select all', on_click=lambda: self.ag_grid.call_api_method('selectAll'))
         if lod is not None:
             self.load_lod(lod, columnDefs)
      
@@ -32,7 +37,43 @@ class ListOfDictsGrid:
         #   self.agGrid.on('rowSelected', self.onRowSelected)
         #self.ag_grid.options["columnDefs"][0]["checkboxSelection"] = True
 
-   
+    def update_index(self,lenient:bool=False):
+        """
+        update the index based on the given key column
+        """
+        self.lod_index={}
+        for row_index,row in enumerate(self.lod):
+            if self.key_col in row:
+                key_value=row[self.key_col]
+                self.lod_index[key_value]=row
+            else:
+                msg=f"missing key column {self.key_col} in row {row_index}"
+                if not lenient:
+                    raise Exception(msg)
+                else:
+                    print(msg,file=sys.stderr)
+                # missing key
+                pass
+
+    def get_row_for_key(self, key_value:str):
+        row=self.lod_index.get(key_value,None)
+        return row
+
+    def update_row(self, key_value, row_key, value):
+        """
+        Update a row in the grid.
+        """
+        row=self.get_row_for_key(key_value)
+        if row:
+            row_data=self.get_row_data()
+            for grid_row in row_data:
+                if row[self.key_col] == grid_row[self.key_col]:
+                    grid_row[row_key] = value
+            
+    def get_row_data(self):
+        row_data=self.ag_grid.options["rowData"]
+        return row_data
+    
     async def onSizeColumnsToFit(self,_msg:dict):      
         await asyncio.sleep(0.2)
         if self.ag_grid:
@@ -75,4 +116,8 @@ class ListOfDictsGrid:
                     columnDefs.append(dict({'field': key, "filter": col_filter}))
         self.ag_grid.options["columnDefs"]=columnDefs
         self.ag_grid.options["rowData"]=lod
+        self.update_index(lenient=self.lenient)
+        self.update()
+        
+    def update(self):
         self.ag_grid.update()
---
+++ tests/test_lod_grid.py
@@ -0,0 +1,39 @@
+'''
+Created on 2023-11-02
+
+@author: wf
+'''
+from tests.basetest import Basetest
+from ngwidgets.lod_grid import ListOfDictsGrid
+
+class TestLodGrid(Basetest):
+    '''
+    test ListOfDictsGrid
+    '''
+    def setUp(self, debug=False, profile=True):
+        Basetest.setUp(self, debug=debug, profile=profile)
+        self.lod=[
+            {'name': 'Alice', 'age': 18, 'parent': 'David'},
+            {'name': 'Bob', 'age': 21, 'parent': 'Eve'},
+            {'name': 'Carol', 'age': 42, 'parent': 'Frank'},
+        ]
+        self.lod_grid=ListOfDictsGrid(self.lod,key_col="name")
+
+    def test_lod_index(self):
+        """
+        test the list of dics indexing
+        """
+        debug=self.debug
+        #debug=True
+        if debug:
+            print(self.lod_grid.lod_index)
+        self.assertTrue("Alice" in self.lod_grid.lod_index)
+            
+
+    def test_update_row(self):
+        new_age=19
+        self.lod_grid.update_row("Alice","age",new_age)
+        alice_row=self.lod_grid.lod_index["Alice"]
+        self.assertEqual(new_age,alice_row["age"])
+    
+        
