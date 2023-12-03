ef077671f78b83c49f384e469ad7d6ac02512619
Wolfgang Fahl <wf@bitplan.com>
2023-12-03 12:26:45 +0100

    fixes #45

--- ngwidgets/progress.py
+++ ngwidgets/progress.py
@@ -1,83 +1,77 @@
-'''
-Created on 2023-09-12
-
-@author: wf
-'''
 from tqdm import tqdm
 from nicegui import ui
 from dataclasses import dataclass
 
 @dataclass
-class Progressbar():
+class Progressbar:
     """
-    generic progress bar 
+    Generic progress bar.
     """
-    total: int
+    _total: int
     desc: str
     unit: str
-  
+
+    @property
+    def total(self) -> int:
+        return self._total
+
+    @total.setter
+    def total(self, total: int):
+        self._total = total
+        self.update_total()
+
+    def update_total(self):
+        """
+        Update the total value in the progress bar.
+        """
+        pass
+
 class NiceguiProgressbar(Progressbar):
     """
-    nicegui progess bar wrapper
+    Nicegui progress bar wrapper.
     """
 
-    def __init__(self,total,desc:str,unit:str):
-        """
-        construct the progress bar
-        """
-        Progressbar.__init__(self,total,desc,unit)
-        self.value=0
-        self.desc=desc
+    def __init__(self, total, desc: str, unit: str):
+        super().__init__(total, desc, unit)
+        self.value = 0
         self.progress = ui.linear_progress(value=0).props('instant-feedback')
         with self.progress:
             self.label = ui.label().classes('text-lg text-white absolute-center') 
             self.label.bind_text_from(self, 'value', backward=lambda v: f'{self.desc} {v}/{self.total}')
-   
         self.progress.visible = False
-        
+
     def reset(self):
-        """
-        reset the progress bar
-        """
-        self.value=0
-        self.progress.value=0
-        
-    def set_description(self,desc:str):
-        """
-        set the description of the progress bar
-        """
-        self.desc=desc
-        self.progress.visible=True
-        pass
-        
-    def update(self,step):
-        """
-        update the progress bar
-        """
-        self.value+=step
-        self.progress.visible=True
-        percent=round(self.value/self.total,2) # rounded to two digits
-        self.progress.value=percent  #round(percent*100)
-        pass
-    
+        self.value = 0
+        self.progress.value = 0
+
+    def set_description(self, desc: str):
+        self.desc = desc
+        self.progress.visible = True
+
+    def update(self, step):
+        self.value += step
+        self.progress.visible = True
+        percent = round(self.value / self.total, 2)
+        self.progress.value = percent
+
 class TqdmProgressbar(Progressbar):
     """
-    tqdm progress bar wrapper
+    Tqdm progress bar wrapper.
     """
-    
-    def __init__(self,total,desc,unit):
-        """
-        constructor
-        """
-        Progressbar.__init__(self,total,desc,unit)
+
+    def __init__(self, total, desc, unit):
+        super().__init__(total, desc, unit)
         self.reset()
-        
+
     def reset(self):
-        self.progress=tqdm(total=self.total, desc=self.desc, unit=self.unit)
-        
-    def set_description(self,desc:str):
+        self.progress = tqdm(total=self.total, desc=self.desc, unit=self.unit)
+
+    def set_description(self, desc: str):
         self.progress.set_description(desc)
-        
-    def update(self,step):
+
+    def update(self, step):
         self.progress.update(step)
-    
+
+    def update_total(self):
+        self.progress.total = self.total
+        self.progress.refresh()
