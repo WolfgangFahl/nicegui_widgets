e4dbbe82c6fff42c3072e848ba8f1e3af3ea9ab8
Wolfgang Fahl <wf@bitplan.com>
2024-01-21 20:02:35 +0100

    version hell

--- ngwidgets/version.py
+++ ngwidgets/version.py
@@ -17,7 +17,7 @@ class Version:
     name = "ngwidgets"
     version = ngwidgets.__version__
     date = "2023-09-10"
-    updated = "2024-01-15"
+    updated = "2024-01-21"
     description = "NiceGUI widgets"
 
     authors = "Wolfgang Fahl"
