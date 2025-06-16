3baac07fda34fc1f092d2e8e992b2fdb5d4b1a3c
Wolfgang Fahl <wf@bitplan.com>
2025-06-16 14:19:53 +0200

    refactors

--- ngwidgets/cmd.py
+++ ngwidgets/cmd.py
@@ -4,22 +4,17 @@ Created on 2023-09-10
 @author: wf
 """
 
-# avoid ugly deprecation messages see
-# https://stackoverflow.com/questions/879173/how-to-ignore-deprecation-warnings-in-python
-# and
-import shutup
 
-shutup.please()
 import sys
 import traceback
 import webbrowser
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from datetime import datetime
-
+from basemkit.base_cmd import BaseCmd
 from ngwidgets.webserver import WebserverConfig
 
 
-class WebserverCmd(object):
+class WebserverCmd(BaseCmd):
     """
     Baseclass for command line handling of Webservers
     """
@@ -28,119 +23,75 @@ class WebserverCmd(object):
         """
         constructor
         """
+        super().__init__(config.version, config.version.description)
         self.config = config
         self.version = config.version
         self.debug = debug
         self.webserver_cls = webserver_cls
-        self.exit_code = 0
 
-    def getArgParser(self, description: str = None, version_msg=None) -> ArgumentParser:
+    def add_arguments(self, parser: ArgumentParser):
         """
-        Setup command line argument parser
+        Add webserver-specific CLI arguments.
 
         Args:
-            description(str): the description
-            version_msg(str): the version message
-
-        Returns:
-            ArgumentParser: the argument parser
+            parser (ArgumentParser): the parser to add arguments to.
         """
-        if description is None:
-            description = self.version.description
-        if version_msg is None:
-            version_msg = self.program_version_message
-        parser = ArgumentParser(
-            description=description, formatter_class=RawDescriptionHelpFormatter
-        )
-        parser.add_argument(
-            "-a",
-            "--about",
-            help="show about info [default: %(default)s]",
-            action="store_true",
-        )
+        super().add_arguments(parser)
         parser.add_argument(
             "--apache",
-            help="create an apache configuration file for the given domain",
+            help="create an apache configuration file for the given domain"
         )
         parser.add_argument(
-            "-c",
-            "--client",
+            "-c", "--client",
             action="store_true",
-            help="start client [default: %(default)s]",
+            help="start client"
         )
         parser.add_argument(
-            "-d",
-            "--debug",
+            "-l", "--local",
             action="store_true",
-            help="show debug info [default: %(default)s]",
-        )
-        parser.add_argument("--debugServer", help="remote debug Server")
-        parser.add_argument(
-            "--debugPort", type=int, help="remote debug Port", default=5678
+            help="run with local file system access"
         )
         parser.add_argument(
-            "--debugRemotePath",
-            help="remote debug Server path mapping - remotePath - path on debug server",
+            "-i", "--input",
+            help="input file"
         )
         parser.add_argument(
-            "--debugLocalPath",
-            help="remote debug Server path mapping - localPath - path on machine where python runs",
-        )
-
-        parser.add_argument(
-            "-l",
-            "--local",
+            "-rol", "--render_on_load",
             action="store_true",
-            help="run with local file system access [default: %(default)s]",
+            help="render on load"
         )
-        parser.add_argument("-i", "--input", help="input file")
-
-        parser.add_argument(
-            "-rol",
-            "--render_on_load",
-            action="store_true",
-            help="render on load [default: %(default)s]",
-        )
-
         parser.add_argument(
             "--host",
             default="localhost",
-            help="the host to serve / listen from [default: %(default)s]",
+            help="the host to serve / listen from (default: localhost)"
         )
         parser.add_argument(
             "--port",
             type=int,
             default=self.config.default_port,
-            help="the port to serve from [default: %(default)s]",
+            help=f"the port to serve from (default: {self.config.default_port})"
         )
         parser.add_argument(
-            "-s",
-            "--serve",
+            "-s", "--serve",
             action="store_true",
-            help="start webserver [default: %(default)s]",
+            help="start webserver"
         )
-        parser.add_argument("-V", "--version", action="version", version=version_msg)
-        return parser
 
-    def handle_args(self) -> bool:
-        handled = False
-        if self.args.apache:
-            print(self.to_apache_config(self.config, self.args.apache))
-        if self.args.about:
-            print(self.program_version_message)
-            print(f"see {self.version.doc_url}")
-            webbrowser.open(self.version.doc_url)
-            handled = True
-        if self.args.client:
-            url = f"http://{self.args.host}:{self.args.port}"
+    def handle_args(self, args):
+        if super().handle_args(args):
+            return True
+        if args.apache:
+            print(self.to_apache_config(self.config, args.apache))
+            return True
+        if args.client:
+            url = f"http://{args.host}:{args.port}"
             webbrowser.open(url)
-            handled = True
-        if self.args.serve:
-            # instantiate the webserver
+            return True
+        if args.serve:
             ws = self.webserver_cls()
-            ws.run(self.args)
-            handled = True
-        return handled
+            ws.run(args)
+            return True
+        return False
 
     def to_apache_config(self, config: WebserverConfig, domain: str) -> str:
         """
@@ -211,57 +162,10 @@ class WebserverCmd(object):
         apache_config = header_comment + ssl_config + http_config
         return apache_config
 
-    def cmd_parse(self, argv: list = None):
+    @classmethod
+    def main(cls, config: WebserverConfig, webserver_cls, argv=None) -> int:
         """
-        parse the argument lists and prepare
-
-        Args:
-            argv(list): list of command line arguments
-
+        Main entry point to launch the webserver cmd with BaseCmd logic.
         """
-        if argv is None:
-            argv = sys.argv[1:]
-        self.argv = argv
-        self.program_name = self.version.name
-        self.program_version = f"v{self.version.version}"
-        self.program_build_date = str(self.version.date)
-        self.program_version_message = (
-            f"{self.program_name} ({self.program_version},{self.program_build_date})"
-        )
-        self.parser = self.getArgParser(
-            description=self.version.description,
-            version_msg=self.program_version_message,
-        )
-        self.args = self.parser.parse_args(argv)
-        return self.args
-
-    def cmd_main(self, argv: None) -> int:
-        """
-        main program as an instance
-
-        Args:
-            argv(list): list of command line arguments
-
-        Returns:
-            int: exit code - 0 of all went well 1 for keyboard interrupt and 2 for exceptions
-        """
-        try:
-            self.cmd_parse(argv)
-            if len(self.argv) < 1:
-                self.parser.print_usage()
-                sys.exit(1)
-            self.handle_args()
-        except KeyboardInterrupt:
-            ### handle keyboard interrupt ###
-            self.exit_code = 1
-        except Exception as e:
-            if self.debug:
-                raise (e)
-            indent = len(self.program_name) * " "
-            sys.stderr.write(self.program_name + ": " + repr(e) + "\n")
-            sys.stderr.write(indent + "  for help use --help")
-            if self.args.debug:
-                print(traceback.format_exc())
-            self.exit_code = 2
-
-        return self.exit_code
+        instance = cls(config=config, webserver_cls=webserver_cls)
+        return instance.run(argv)
